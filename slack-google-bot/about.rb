150f8378bbe8131429d2df901466e9016ca3a862
jerryphan2009 <36296085+jerryphan2009@users.noreply.github.com>
2018-02-09 20:18:51 +0700

    Update about.rb

--- slack-google-bot/about.rb
+++ slack-google-bot/about.rb
@@ -2,6 +2,7 @@ module SlackGoogleBot
   ABOUT = <<-ABOUT
     #{SlackGoogleBot::VERSION}
     https://github.com/dblock/slack-google-bot
+    Test cho QWE coi nó có ra ko đkmtb
     https://twitter.com/dblockdotorg
   ABOUT
 end
